3d82f8e3b38580acb4cd88f930ecc6118630f546
aaldu <aaldu@DESKTOP-26OB3J1.myfiosgateway.com>
2024-01-03 21:00:29 -0500

    Initial Message

---
+++ src/e/DemoE.java
@@ -0,0 +1,13 @@
+package e;
+
+public class DemoE {
+	
+	public static void main(String[] args) {
+		System.out.println("Friday");
+		
+		
+		System.out.println("Batch11");
+		System.out.println("ProjectE");
+	}
+
+}
